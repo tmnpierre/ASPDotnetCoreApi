9957481eb4d85594daa067569df62c6f423b0a41
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 14:44:19 +0100

    add CrepeFakeDb

--- Demo01/Data/CrepeFakeDb.cs
+++ Demo01/Data/CrepeFakeDb.cs
@@ -1,6 +1,27 @@
-﻿namespace Demo01.Data
+﻿using Demo01.Models;
+
+namespace Demo01.Data
 {
     public class CrepeFakeDb
     {
+        public List<Crepe>? Crepes { get; set; } = new List<Crepe>
+        {
+            new Crepe()
+            {
+                Id = 1,
+                Name = "Surprise du chef",
+                Diameter = 14,
+                Topping1 = Topping.Camembert,
+                Topping2 = Topping.MappleJuice,
+            },
+            new Crepe()
+            {
+                Id = 1,
+                Name = "Fuit de la mer",
+                Diameter = 14,
+                Topping1 = Topping.Surimi,
+                Topping2 = Topping.Nutella,
+            }
+        };
     }
 }
