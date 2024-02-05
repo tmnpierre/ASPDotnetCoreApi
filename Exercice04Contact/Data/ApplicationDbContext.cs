e09222feb899198b3066382e0e5ece2c03736481
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 16:50:40 +0100

    feat(Exercice04Contacts): add ApplicationDbContext

---
+++ Exercice04Contact/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Exercice04Contact.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercice04Contact.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Contact>? Contacts { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
