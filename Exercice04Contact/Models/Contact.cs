902933e13233d60cdeec0affeea016eecb5861a7
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 16:42:35 +0100

    feat(Exercice04Contacts): add Contact model

---
+++ Exercice04Contact/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercice04Contact.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Avatar { get; set; }
+    }
+
+    public enum Gender { Homme, Femme, Autres}
+}
