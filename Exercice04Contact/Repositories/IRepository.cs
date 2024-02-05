596a719a36d6c47eb00884e352599fab7ace7cc2
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 19:08:30 +0100

    feat(repositories): add IRepository

---
+++ Exercice04Contact/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercice04Contact.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        bool Create (T entity);
+
+        T GetById(int id);
+
+        List<T> GetAll();
+
+        bool Update (T entity);
+
+        bool Delete (T entity);
+    }
+}
