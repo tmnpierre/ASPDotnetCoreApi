e26c468b76857a5ab25276fb5b9dcafdcbaaffbd
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 18:37:00 +0100

    feat(models): update Contact

--- Exercice04Contact/Models/Contact.cs
+++ Exercice04Contact/Models/Contact.cs
@@ -1,15 +1,32 @@
-﻿namespace Exercice04Contact.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exercice04Contact.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string? LastName { get; set; }
-        public int DateOfBirth { get; set; }
-        public int Age { get; set; }
-        public Gender? Gender { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age => DateTime.Today.Year - DateOfBirth.Year - (DateTime.Today.DayOfYear < DateOfBirth.DayOfYear ? 1 : 0);
+
+        public Gender Gender { get; set; }
+
+        [Url]
         public string? Avatar { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 
-    public enum Gender { Homme, Femme, Autres}
+    public enum Gender { Homme, Femme, Autres }
 }
+
