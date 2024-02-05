e89b4fdbfec63b7792acbe6f3c0e5515100323c1
tmnpierre <tomyn.pierre@gmail.com>
2024-02-05 18:43:35 +0100

    feat(data): update ApplicationDbContext

--- Exercice04Contact/Data/ApplicationDbContext.cs
+++ Exercice04Contact/Data/ApplicationDbContext.cs
@@ -8,5 +8,18 @@ namespace Exercice04Contact.Data
         public DbSet<Contact>? Contacts { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.DateOfBirth).IsRequired();
+                entity.Property(e => e.Gender).HasConversion<string>();
+                entity.Property(e => e.Avatar).IsRequired(false);
+            });
+        }
     }
 }
